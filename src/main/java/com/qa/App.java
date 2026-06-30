a70a8002dbd2bb3ee4af92690ceb76c68de0c5ab
Richard <rpeel1234@gmail.com>
2026-06-30 14:27:40 +0100

    RELEASE:Version 1.0 release

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,6 +12,8 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Mohit"));
+        System.out.println(sayHelloToSomeone("Richard"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
